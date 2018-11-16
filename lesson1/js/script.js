8f5c6bcff41ac325c18ef41f7bd0b636e7318af4
Tamara Webdev <tamara.webdev@gmail.com>
2018-11-16 10:55:17 +0100

    Supplement commit

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -3,16 +3,17 @@
 let money = prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-let mustHaveExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
-let willCost = prompt("Во сколько обойдется?", "");
+
+let spending1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let willCost1 = prompt("Во сколько обойдется?", "");
+let spending2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let willCost2 = prompt("Во сколько обойдется?", "");
 
 
 let appData = {
     budget: money,
     timeData: time,
-    //expenses: {mustHaveExpenses: "", willCost: ""}, // Вариант 1
-    //expenses: {"обязательная статья": "", "willCost": ""}, // Вариант 2
-    expenses: {"обязательная статья": mustHaveExpenses, "статья 2": willCost}, // Вариант 3. Какой лучше? Если они вообще правильные)
+    expenses: {spending1: willCost1, spending2: willCost2},
     optionalExpenses: {"": ""},
     income: [],
     savings: false
@@ -21,4 +22,4 @@ let appData = {
 
 alert(appData.budget / 30);
 
-//console.log(appData);
+console.log(appData);
