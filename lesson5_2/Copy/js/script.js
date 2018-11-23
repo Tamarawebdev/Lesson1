89ac91add07069c3ee7563650ec5428f87f70e4c
Tamara Webdev <tamara.webdev@gmail>
2018-11-23 20:48:37 +0100

    Lesson5_2

---
+++ lesson5_2/Copy/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let idStart = document.querySelector("#start"),
+    classAndValue = document.querySelectorAll(".budget-value, .daybudget-value, .level-value, .expenses-value, .optionalexpenses-value, .income-value, .monthsavings-value, .yearsavings-value"),
+    expensesClass = document.querySelector(".expenses-item"),
+    expensesBtn = document.getElementsByTagName("button"),
+    countBtn = document.getElementsByTagName("button"),
+    optionalExpensesClass = document.querySelectorAll(".optionalexpenses-item"),
+    incomeId = document.querySelector("#income"),
+    savingsId =document. querySelector("#savings"),
+    sumId = document.querySelector("#sum"),
+    percentId = document.querySelector("#percent"),
+    yearClass = document.querySelector(".year"),
+    monthClass = document.querySelector(".month"),
+    dayClass = document.querySelector(".day");
+
