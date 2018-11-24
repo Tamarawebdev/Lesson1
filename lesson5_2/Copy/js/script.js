81e6844c32071f09166c3a5ec9080fbc1428d696
Tamara Webdev <tamara.webdev@gmail>
2018-11-24 07:35:54 +0100

    Updated Lesson5_2

--- lesson5_2/Copy/js/script.js
+++ lesson5_2/Copy/js/script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-let idStart = document.querySelector("#start"),
+let idStart = document.getElementById("#start"),
     classAndValue = document.querySelectorAll(".budget-value, .daybudget-value, .level-value, .expenses-value, .optionalexpenses-value, .income-value, .monthsavings-value, .yearsavings-value"),
-    expensesClass = document.querySelector(".expenses-item"),
+    expensesClass = document.getElementsByClassName(".expenses-item"),
     expensesBtn = document.getElementsByTagName("button"),
     countBtn = document.getElementsByTagName("button"),
-    optionalExpensesClass = document.querySelectorAll(".optionalexpenses-item"),
-    incomeId = document.querySelector("#income"),
-    savingsId =document. querySelector("#savings"),
-    sumId = document.querySelector("#sum"),
-    percentId = document.querySelector("#percent"),
-    yearClass = document.querySelector(".year"),
-    monthClass = document.querySelector(".month"),
-    dayClass = document.querySelector(".day");
+    optionalExpensesClass = document.getElementsByClassName(".optionalexpenses-item"),
+    incomeId = document.getElementById("#income"),
+    savingsId =document.getElementById("#savings"),
+    sumId = document.getElementById("#sum"),
+    percentId = document.getElementById("#percent"),
+    yearClass = document.getElementsByClassName(".year"),
+    monthClass = document.getElementsByClassName(".month"),
+    dayClass = document.getElementsByClassName(".day");
 
