5c70ba808e49074c2ef1f7436b118880ffac3682
Tamara Webdev <tamara.webdev@gmail>
2018-11-29 18:40:44 +0100

    Lesson 7

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -8,9 +8,6 @@ function timer() {
 	    minutes = time.getMinutes().toString(),
 	    seconds = time.getSeconds().toString();
 
-    let r = Math.round(hour * 11.087).toString(16),
-	    g = Math.round(minutes * 4.32).toString(16),
-		b = Math.round(seconds * 4.32).toString(16);
 
 	if (hour.length < 2) {
 		hour = "0" + hour;
@@ -22,15 +19,6 @@ function timer() {
 		seconds = "0" + seconds;
 	}
 
-	if (r.length === 1) {
-		r = "0" + r;
-	}
-	if (g.length === 1) {
-		g = "0" + g;
-	}
-	if (b.length === 1) {
-		b = "0" + b;
-	}
 
 	let clockString = hour + ":" + minutes + ":" + seconds;
 
